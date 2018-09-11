36b38633cbdd456e72a43de22b3446e52cb4f2d8
Oratree <neem693@gmail.com>
2018-09-11 15:23:00 +0900

    update

---
+++ last_basball_project/src/main/java/kr/co/pickBaseball/JoonggoController.java
@@ -0,0 +1,23 @@
+package kr.co.pickBaseball;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+public class JoonggoController {
+
+	@RequestMapping("/joonggo/insert_form.do")
+	public String insert_form() {
+		return myconst.Myconst.Joonggo.VIEW_PATH + "joonggo_insert_form.jsp";
+	}
+
+	@RequestMapping("/joonggo/list.do")
+	public String list() {
+		return myconst.Myconst.Joonggo.VIEW_PATH + "joonggo_list.jsp";
+
+	}
+}
---
+++ last_basball_project/src/main/java/myconst/Myconst.java
@@ -0,0 +1,31 @@
+package myconst;
+
+public class Myconst {
+
+	public static class BaseBall {
+		public static String BASEBALL_DIR = "/WEB-INF/views/pickBaseball/";
+
+	}
+
+	public static class Member {
+		public static String MEMBER_DIR = "/WEB-INF/views/pickBaseball/input/";
+
+	}
+
+	public static class ParsingDateCheck {
+		public static int YEAR = 2018;
+		public static int PARSING_INTERVAL = 1;
+		public static int ONE_HOUR_MILESECOND = 3600000;
+	}
+
+
+
+	public static class DateCheck {
+		public static int year = 2018;
+	}
+
+	public static class Joonggo {
+		public static String VIEW_PATH = "/WEB-INF/views/joonggo/";
+	}
+
+}
